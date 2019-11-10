9b3b734ebb67fa24b6da0ddb50875c18cdc21cd6
darren-rose <darrenwrose@gmail.com>
2019-11-10 18:36:39 +0700

    polish

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package hello
 
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func Hello() string {
-    return quote.Hello()
+	str := quote.Hello()
+	fmt.Printf("quote.Hello() returned %s\n", str)
+	return str
 }
