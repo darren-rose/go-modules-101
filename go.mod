a1c3509585e2a34b2eed82a56f1eb1eba38096f8
darren-rose <darrenwrose@gmail.com>
2019-11-11 18:29:28 +0700

    add Proverb support

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	golang.org/x/text v0.3.2 // indirect
 	golang.org/x/tools v0.0.0-20191109212701-97ad0ed33101 // indirect
 	rsc.io/quote v1.5.2
+	rsc.io/quote/v3 v3.1.0
 	rsc.io/sampler v1.3.1 // indirect
 )
--- hello.go
+++ hello.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
 )
 
 func Hello() string {
 	str := quote.Hello()
 	fmt.Printf("quote.Hello() returned %s\n", str)
+	return str
+}
+
+func Proverb() string {
+	str := quoteV3.Concurrency()
+	fmt.Printf("quoteV3.Concurrency() returned %s\n", str)
 	return str
 }
